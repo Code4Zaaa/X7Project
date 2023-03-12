7ecb88e432ebd6ff5985d0f70d65fa8b9ecd9924
vyn <98988528+x7Vynth@users.noreply.github.com>
2023-03-12 18:50:44 +0700

    Update ThemeManager.lua

--- addons/ThemeManager.lua
+++ addons/ThemeManager.lua
@@ -1,11 +1,11 @@
 local httpService = game:GetService('HttpService')
 local ThemeManager = {} do
-	ThemeManager.Folder = 'Catalyst Hub Premium Scripts'
+	ThemeManager.Folder = 'X7Project Addons'
 	-- if not isfolder(ThemeManager.Folder) then makefolder(ThemeManager.Folder) end
 
 	ThemeManager.Library = nil
 	ThemeManager.BuiltInThemes = {
-		['Catalyst Hub'] 		= { 1, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"242323","AccentColor":"7ccfff","BackgroundColor":"2f2b2b","OutlineColor":"302b2b"}') },
+		['X7 Projects'] 		= { 1, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"242323","AccentColor":"7ccfff","BackgroundColor":"2f2b2b","OutlineColor":"302b2b"}') },
 		['Dracula'] 		= { 2, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"232533","AccentColor":"6271a5","BackgroundColor":"1b1c27","OutlineColor":"7c82a7"}') },
 		['Bitch Bot'] 		= { 3, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"1e1e1e","AccentColor":"7e48a3","BackgroundColor":"232323","OutlineColor":"141414"}') },
 		['Kiriot Hub'] 		= { 4, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"30333b","AccentColor":"ffaa00","BackgroundColor":"1a1c20","OutlineColor":"141414"}') },
